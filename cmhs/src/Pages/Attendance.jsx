a82143e0856a73c7fa4ef7e5f3bfd2703d739775
Mazharul Islam <engrreza4@gmail.com>
2023-09-05 22:39:35 +0600

    update

---
+++ cmhs/src/Pages/Attendance.jsx
@@ -0,0 +1,9 @@
+
+
+export default function Attendance() {
+  return (
+    <div className="py-6 text-center text-xl md:text-2xl lg:text-3xl">
+      শীঘ্রই অনলাইন হাজিরা পদ্ধতি চালু করা হবে ।
+    </div>
+  );
+}
---
+++ cmhs/src/Pages/Footer.jsx
@@ -0,0 +1,193 @@
+export default function Footer() {
+  return (
+    <div className="px-4 lg:px-[60px] xl:px-[100px]">
+      <div className="grid  md:grid-cols-3 gap-6 md:pb-6 lg:pb-12">
+        <div>
+          <div className="bg-violet-900 text-white text-center py-2 px-4 mb-4">
+            <p>গুরুত্বপূর্ণ লিংক</p>
+          </div>
+          <div className="space-y-0 text-sm md:text-base md:space-y-2 px-4">
+            <a
+              href="https://moedu.gov.bd/"
+              target="blank"
+              className="flex gap-2 text-blue-700">
+              <div className="text-xl">
+                <ion-icon name="checkmark-done-circle-outline"></ion-icon>
+              </div>
+              <p className="text-gray-500">শিক্ষা মন্ত্রণালয়</p>
+            </a>
+            <a
+              href="https://dshe.gov.bd/"
+              target="blank"
+              className="flex gap-2 text-blue-700">
+              <div className="text-xl">
+                <ion-icon name="checkmark-done-circle-outline"></ion-icon>
+              </div>
+              <p className="text-gray-500">মাধ্যমিক ও উচ্চ শিক্ষা অধিদপ্তর</p>
+            </a>
+            <a
+              href="http://www.emis.gov.bd/emis"
+              target="blank"
+              className="flex gap-2 text-blue-700">
+              <div className="text-xl">
+                <ion-icon name="checkmark-done-circle-outline"></ion-icon>
+              </div>
+              <p className="text-gray-500">EMIS</p>
+            </a>
+            <a
+              href="http://www.ntrca.gov.bd/"
+              target="blank"
+              className="flex gap-2 text-blue-700">
+              <div className="text-xl">
+                <ion-icon name="checkmark-done-circle-outline"></ion-icon>
+              </div>
+              <p className="text-gray-500">NTRCA</p>
+            </a>
+            <a
+              href="https://banbeis.gov.bd/"
+              target="blank"
+              className="flex gap-2 text-blue-700">
+              <div className="text-xl">
+                <ion-icon name="checkmark-done-circle-outline"></ion-icon>
+              </div>
+              <p className="text-gray-500">ব্যানবেইজ</p>
+            </a>
+          </div>
+        </div>
+        <div>
+          <div className="bg-violet-900 text-white text-center py-2 px-4 mb-4">
+            <p>অন্যান্য</p>
+          </div>
+          <div className="space-y-0 text-sm md:text-base md:space-y-2 px-4">
+            <a
+              href="https://bangladesh.gov.bd/index.php"
+              target="blank"
+              className="flex gap-2 text-blue-700">
+              <div className="text-xl">
+                <ion-icon name="checkmark-done-circle-outline"></ion-icon>
+              </div>
+              <p className="text-gray-500">জাতীয় তথ্য বাতায়ন</p>
+            </a>
+            <a
+              href="https://muktopaath.gov.bd/"
+              target="blank"
+              className="flex gap-2 text-blue-700">
+              <div className="text-xl">
+                <ion-icon name="checkmark-done-circle-outline"></ion-icon>
+              </div>
+              <p className="text-gray-500">মুক্তপাঠ</p>
+            </a>
+            <a
+              href="https://www.tangail.gov.bd/"
+              target="blank"
+              className="flex gap-2 text-blue-700">
+              <div className="text-xl">
+                <ion-icon name="checkmark-done-circle-outline"></ion-icon>
+              </div>
+              <p className="text-gray-500">টাঙ্গাইল জেলা</p>
+            </a>
+            <a
+              href="https://tangailsadar.tangail.gov.bd/"
+              target="blank"
+              className="flex gap-2 text-blue-700">
+              <div className="text-xl">
+                <ion-icon name="checkmark-done-circle-outline"></ion-icon>
+              </div>
+              <p className="text-gray-500">টাঙ্গাইল সদর উপজেলা</p>
+            </a>
+            <a
+              href="https://baghilup.tangail.gov.bd/en"
+              target="blank"
+              className="flex gap-2 text-blue-700">
+              <div className="text-xl">
+                <ion-icon name="checkmark-done-circle-outline"></ion-icon>
+              </div>
+              <p className="text-gray-500">বাঘিল ইউনিয়ন</p>
+            </a>
+          </div>
+        </div>
+        <div>
+          <div className="bg-violet-900 text-white text-center py-2 px-4 mb-4">
+            <p>শিক্ষাবোর্ড</p>
+          </div>
+          <div className="space-y-0 text-sm md:text-base md:space-y-2 px-4">
+            <a
+              href="https://dhakaeducationboard.gov.bd/"
+              target="blank"
+              className="flex gap-2 text-blue-700">
+              <div className="text-xl">
+                <ion-icon name="checkmark-done-circle-outline"></ion-icon>
+              </div>
+              <p className="text-gray-500">ঢাকা শিক্ষাবোর্ড</p>
+            </a>
+            <a
+              href="https://dhakaeducationboard.gov.bd/"
+              target="blank"
+              className="flex gap-2 text-blue-700">
+              <div className="text-xl">
+                <ion-icon name="checkmark-done-circle-outline"></ion-icon>
+              </div>
+              <p className="text-gray-500">শিক্ষাবোর্ড, ঢাকা</p>
+            </a>
+            <a
+              href="https://rajshahieducationboard.gov.bd/"
+              target="blank"
+              className="flex gap-2 text-blue-700">
+              <div className="text-xl">
+                <ion-icon name="checkmark-done-circle-outline"></ion-icon>
+              </div>
+              <p className="text-gray-500">রাজশাহী শিক্ষাবোর্ড</p>
+            </a>
+            <a
+              href="http://www.dinajpureducationboard.gov.bd/"
+              target="blank"
+              className="flex gap-2 text-blue-700">
+              <div className="text-xl">
+                <ion-icon name="checkmark-done-circle-outline"></ion-icon>
+              </div>
+              <p className="text-gray-500">দিনাজপুর শিক্ষাবোর্ড</p>
+            </a>
+
+            <a
+              href="http://www.educationboardresults.gov.bd/"
+              target="blank"
+              className="flex gap-2 text-blue-700">
+              <div className="text-xl">
+                <ion-icon name="checkmark-done-circle-outline"></ion-icon>
+              </div>
+              <p className="text-gray-500">বোর্ড রেজাল্ট</p>
+            </a>
+          </div>
+        </div>
+      </div>
+
+      <div>
+        <img src="footer.png" className="w-full" alt="" />
+        <div className="p-4 bg-black">
+          <div className="grid md:grid-cols-3 space-y-4 md:space-y-0 items-start text-white text-center">
+            <div className="md:space-y-2">
+              <p>পরিকল্পনা ও বাস্তবায়নে:</p>
+              <p>মোঃ সেলিম আল-মামুন</p>
+              <p>অফিস সহকারী কাম কম্পিউটার অপারেটর</p>
+              <p>ধরেরবাড়ি মুসলিম হাই স্কুল এন্ড কলেজ</p>
+            </div>
+            <div className="md:space-y-2">
+              <p>ব্যবস্থাপনা ও তত্ত্বাবধানে:</p>
+              <p>আলহাজ মোঃ শহিদুল ইসলাম</p>
+              <p>প্রধান শিক্ষক / অধ্যক্ষ (ভারপ্রাপ্ত)</p>
+              <p>ধরেরবাড়ি মুসলিম হাই স্কুল এন্ড কলেজ</p>
+            </div>
+            <div className="md:space-y-2">
+              <p>কারিগরি সহায়তায়:</p>
+
+              <a href="https://mazharraj.netlify.app/" target="blank">
+                <p>মাজহারুল ইসলাম</p>
+              </a>
+              <p className="text-gray-500">কপিরাইট ©২০২৩ সর্বস্বত্ব সংরক্ষিত</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
